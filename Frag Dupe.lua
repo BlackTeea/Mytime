cf7d771ebc25cdd5dd345947b7085d6faa179587
BlackTeea <floreselion123@gmail.com>
2024-07-07 18:58:33 -0700

    Add files via upload

---
+++ Frag Dupe.lua
@@ -0,0 +1,13 @@
+--@Faxxed, Only Works with shard that you can convert and must must have at least 1 Shard!!
+local items = game:GetService("Players").LocalPlayer.PlayerGui.MainGui.Items.Main.Scroll
+
+for _, v in pairs(items:GetDescendants()) do
+    if string.match(v.name,"Shard") == "Shard" then
+        game:GetService("ReplicatedStorage").Remote.AttemptShardConvert:FireServer(v.name,-99999,"Next")
+    end
+end
+
+
+
+
+
